0f8df6059e172c0d2d42176d6f354029f9a26eb1
swgoodman <spencerwgoodman@icloud.com>
2018-11-18 22:20:11 -0800

    Added private methods to User class

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  def index
-    render json: { success: "ok", status: 200 }
+
+  def new
   end
 
   def create
@@ -15,5 +16,22 @@ class Api::V1::UsersController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+
+  helper_method :set_user
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :username,
+        :email,
+        :password,
+        :image
+        )
+    end
+
 end
