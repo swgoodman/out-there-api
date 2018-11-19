0f19caefb5630a7a5c443e4efba811d654b14aee
swgoodman <spencerwgoodman@icloud.com>
2018-11-18 22:34:16 -0800

    Created Session Controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+    @users = User.all
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user
+      session[:user_id] = @user.id
+      # SEND USER JSON
+    else
+      render :new
+    end
+  end
+
+end
