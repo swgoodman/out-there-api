2ec46d6e13e0173438f8879d12981f7c678b5d7a
swgoodman <spencerwgoodman@icloud.com>
2018-11-18 22:46:06 -0800

    Added comments to Board Controller

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,19 +1,20 @@
 class Api::V1::BoardsController < ApplicationController
 
   def index
+    # Receives GET Request and Responds with Group's Boards
     render json: { success: "ok", status: 200 }
   end
 
   def create
-  end
-
-  def edit
+    # Receives POST Request and Responds with NEW List of Group Boards
   end
 
   def udpate
+    # Receives POST Update Request and Responds with NEW List of Group Boards
   end
 
   def destroy
+    # Receives POST Update Request and Responds with NEW List of Group Boards
   end
 
 end
