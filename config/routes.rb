4b0f7f84173863f1794a200c20aeff4362dca291
swgoodman <spencerwgoodman@icloud.com>
2019-01-23 11:28:33 -0800

    Working on Async loading

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   namespace :api do
     namespace :v1 do
-      resources :boards
-      resources :users do
-        resources :groups, :only => [:index, :create, :show, :update, :destroy]
+      resources :users
+
+      get 'user', to: 'users#show', as: 'user_show'
+      post 'signup', to: 'users#create', as: 'user_signup'
+      post 'login', to: 'users#login', as: 'user_login'
+
+      resources :ideas do
+        resources :comments
       end
     end
   end
-
-
 end
